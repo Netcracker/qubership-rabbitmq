6534e98611f92b12ae5fae599252784e83072876
fibu0125 <filipp.buzid@netcracker.com>
2026-01-06 13:28:02 +0500

    fix: bug with wrong quote function

--- operator/src/rabbit_helper.py
+++ operator/src/rabbit_helper.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from email.utils import quote
-from email.utils import quote
+from urllib.parse import quote
 import requests
 import logging
 from dataclasses import dataclass
