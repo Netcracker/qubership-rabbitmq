471b3be445dbd278a5f1b2f20ce19f2ab43cc854
Clive Dias <clive.dias@netcracker.com>
2026-02-26 13:47:13 +0530

    fix: change self.url

--- operator/src/backup_helper.py
+++ operator/src/backup_helper.py
@@ -31,7 +31,7 @@ class BackupHelper:
         else:
             self.verify = None
             port = "8080"
-        self.url = f'{schema}://{service}.{namespace}:{port}' if not custom_url else custom_url
+        self.url = f'{schema}://{service}:{port}' if not custom_url else custom_url
         self.auth = auth
 
     def _read_backup_daemon_health(self) -> dict:
