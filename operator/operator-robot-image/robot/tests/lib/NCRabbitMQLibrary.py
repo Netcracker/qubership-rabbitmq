7138c0b442a91a7e6a72cc32daf2d9b1e876776c
Clive Dias <clive.dias@netcracker.com>
2026-03-04 17:07:54 +0530

    fix: modify backup

--- operator/operator-robot-image/robot/tests/lib/NCRabbitMQLibrary.py
+++ operator/operator-robot-image/robot/tests/lib/NCRabbitMQLibrary.py
@@ -671,9 +671,10 @@ class NCRabbitMQLibrary(object):
 
     @utils.timeout()
     def make_rabbitmq_full_backup(self):
-        headers = {'Content-Type': 'application/json'}
-        data = '{"allow_eviction":"False"}'
-        res = requests.post(self._backuper_url + "/backup", headers=headers, data=data, verify=self.verify)
+        payload = {
+            "allow_eviction": False
+        }
+        res = requests.post(self._backuper_url + "/backup", json=payload, verify=self.verify)
         return res.text
 
     @utils.timeout()
